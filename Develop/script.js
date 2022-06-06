6abd3aec262db23af898d8dc31376d8060702fee
JM <georgemares14@yahoo.com>
2022-06-06 15:01:21 -0700

    Second Commit. Added checkboxes to index.html for seleecting password options. Added more assignment codes to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var includeSymbols = document.getElementById("symbols");
+var passwordLength = document.getElementById("length");
+var includeNumbers = document.getElementById("numbers");
+var incldeUppercase = document.getElementById("uppercase");
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var uppCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "123456789";
+var symbols = "!@#$%^&*_-+=";
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +18,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
