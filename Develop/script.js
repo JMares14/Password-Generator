ccf07955785763fd943dec34c84d2195ba57064c
JM <georgemares14@yahoo.com>
2022-06-07 14:00:32 -0700

    Added function to generate password. Added function to write password depending on options selected with ternary operators

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordTxt = document.getElementById("password")
 var includeSymbols = document.getElementById("symbols");
 var passwordLength = document.getElementById("length");
 var includeNumbers = document.getElementById("numbers");
@@ -12,14 +13,33 @@ var symbols = "!@#$%^&*_-+=";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var characters = letters;
+  includeNumbers.checked ? (characters += numbers) : "";
+  includeSymbols.checked ? (characters += symbols) : "";
+  incldeUppercase.checked ? (characters += uppCase) : "";
+  passwordTxt.value = generatePassword(passwordLength.value, characters);
+};
 
-  passwordText.value = password;
 
-}
-
+function generatePassword(length, characters) {
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+};
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var characters = letters;
+  includeNumbers.checked ? (characters += numbers) : "";
+  includeSymbols.checked ? (characters += symbols) : "";
+  incldeUppercase.checked ? (characters += uppCase) : "";
+  passwordTxt.value = generatePassword(passwordLength.value, characters);
+};
+
